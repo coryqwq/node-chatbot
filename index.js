7c7e5f96e77d05dd1b1b4efba05a96328d1d0eaf
unknown <cktran02@gmail.com>
2021-08-18 07:33:33 -0700

    update index.js + add .gitignore

--- index.js
+++ index.js
@@ -9,3 +9,29 @@ const server = app.listen(5000);
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
+
+//initialize api.ai with access token
+require('dotenv').config()
+const apiai = require('apiai')(process.env.APIAI_TOKEN);
+
+io.on('connection', function(socket) {
+    socket.on('chat message', (text) => {
+  
+      //get a reply from API.AI
+      let apiaiReq = apiai.textRequest(text, {
+        sessionId: APIAI_SESSION_ID
+      });
+  
+      apiaiReq.on('response', (response) => {
+        let aiText = response.result.fulfillment.speech;
+        socket.emit('bot reply', aiText); //send the result back to the browser!
+      });
+  
+      apiaiReq.on('error', (error) => {
+        console.log(error);
+      });
+  
+      apiaiReq.end();
+  
+    });
+  });
